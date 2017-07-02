8a2a0997806a4166bc237eb79dc1cfa16ccec8ac
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-02 15:15:36 -0400

    initialize to display window

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+import pygame
+
+def run_game():
+    # Initialization of game and screen
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+    pygame.display.set_caption("Alien Puncher")
+    # Primary loop
+    while True:
+        # Events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        # Set display to make the most recently drawn screen visible
+        pygame.display.flip()
+
+run_game()

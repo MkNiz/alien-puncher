0da2d4aaa3f28e7f7ddaedcb3f9233e78b8a846e
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-03 17:04:18 -0400

    Display ship on screen

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # Initialization of game and screen
@@ -11,6 +12,10 @@ def run_game():
         (game_settings.screen_width, game_settings.screen_height)
     )
     pygame.display.set_caption("Alien Puncher")
+
+    # Create the player ship
+    ship = Ship(screen)
+
     # Primary loop
     while True:
         # Events
@@ -19,6 +24,10 @@ def run_game():
                 sys.exit()
         # Set a custom background color
         screen.fill(game_settings.bg_color)
+
+        # Draw the player's ship at its current position
+        ship.blitme()
+
         # Set display to make the most recently drawn screen visible
         pygame.display.flip()
 

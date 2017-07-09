c1d9edecccfbd166c2fb876f150b896ffa550c10
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-09 09:39:13 -0400

    Separation of static and dynamic settings

--- settings.py
+++ settings.py
@@ -9,19 +9,28 @@ class Settings():
         self.bg_color      = (100, 40, 40)
 
         # Ship
-        self.ship_speed_factor = 2.75
         self.ship_limit = 3
 
         # Bullet
-        self.bullet_speed_factor = 8.5
-        self.bullet_side         = "left"
         self.max_bullets         = 2
 
         # Alien
+        self.fleet_drop_speed = 16
+
+        # The speed at which the game becomes faster
+        self.speed_mod = 1.2
+
+        self.init_dynamic_settings()
+
+    def init_dynamic_settings():
+        """Initializes/resets settings that change over the course of the game"""
+        self.ship_speed_factor = 2.75
+        self.bullet_speed_factor = 8.5
+        self.bullet_side = "left"
         self.alien_speed_factor = 2
-        self.fleet_drop_speed = 24
         # Fleet direction; 1 = right, -1 = left
         self.fleet_direction = 1
+
 
     def flip_bullet_side(self):
         """Flips the bullet side between right and left"""

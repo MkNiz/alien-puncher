2d3b9dff27441224bafb361e35debbce41cc2ae9
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-09 09:53:16 -0400

    Scoreboard.prep_score()

--- scoreboard.py
+++ scoreboard.py
@@ -14,3 +14,16 @@ class Scoreboard():
         # Font settings
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
+
+        # Ready the score image
+        self.prep_score()
+
+    def prep_score(self):
+        """Turns the score into a rendered image"""
+        score_str = str(self.stats.score)
+        self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
+
+        # Score is displayed at the top right of the screen
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 16
+        self.score_rect.top = 16
